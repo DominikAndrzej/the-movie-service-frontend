da1589ed5aee7ff3e92a0942f81888efcd8c4688
Dominik Switlik <sd57949@zut.edu.pl>
2026-03-11 10:12:05 +0100

    update accept-language behaviour

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
+import type {AppLanguage} from "@/types/api.ts";
+
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
+  readonly VITE_API_BASE_URL: string,
+  readonly VITE_DEFAULT_LANGUAGE: AppLanguage
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
--- src/appStore.ts
+++ src/appStore.ts
@@ -1,17 +1,22 @@
 import {defineStore} from "pinia";
 import type {AppLanguage} from "@/types/api.ts";
 import {setApiLanguage} from "@/api/client.ts";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 export const useAppStore = defineStore('app', () => {
   // state
-  const language = ref<AppLanguage>('pl');
+  const language = ref<AppLanguage>(import.meta.env.VITE_DEFAULT_LANGUAGE);
+  changeLanguage(import.meta.env.VITE_DEFAULT_LANGUAGE)
 
   // actions
   function changeLanguage(newLang: AppLanguage) {
     language.value = newLang;
-    setApiLanguage(newLang);
   }
+
+  // effects
+  watch(language, (newLang) => {
+    setApiLanguage(newLang);
+  }, { immediate: true });
 
   return {
     language,
